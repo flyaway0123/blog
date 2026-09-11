95591904e5a04a004012535fae19b20969ccf792
refl <18689207943@163.com>
2026-09-12 03:35:01 +0800

    fix: set base and site separately in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,8 @@ import { transformerFileName } from "./src/utils/transformers/fileName";
 import config from "./astro-paper.config";
 
 export default defineConfig({
-  site: config.site.url,
+  site: "https://flyaway0123.github.io",
+  base: "/blog/",
   integrations: [
     mdx(),
     sitemap({
